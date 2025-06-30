787071d7c809557330a8710467b26be6d50ef491
Jordan Richards <jordan.richards@ed.ac.uk>
2025-06-29 22:03:32 -0500

    Translated to keras3

--- keras_check.R
+++
@@ -1,7 +0,0 @@
-# Check if tensorflow is available
-
-reticulate::use_condaenv("USWildfiresExtremes", required = T)
-print("Is Keras available?")
-print(keras::is_keras_available()) #Check if keras is available
-sess = keras::k_get_session()
-print(sess$list_devices())
